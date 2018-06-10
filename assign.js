b1b6c000296a0e65cb7bdb46b8984e1ff4a254cd
Chiman-Dev <bchiman.subudhi@gmail.com>
2018-06-10 18:52:05 +0530

    first commit

---
+++ assign.js
@@ -0,0 +1,29 @@
+console.log("Recipe details")
+console.log("--------------")
+
+var recipeName ="Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter"
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Name: " + recipeName)
+console.log("Preparation Time: " + preparationTime)
+console.log("Recipe By: " + recipeBy)
+console.log("Is Vegetarian: " + isVegetarian)
+console.log("calories: " + calories)
+
+console.log("Recipe details after update")
+console.log("--------------")
+
+var recipeName ="Creamy squash linguine";
+var preparationTime = 60;
+var recipeBy = "Jimmy Cutter George"
+var isVegetarian = true;
+var calories = 223.7;
+
+console.log("Recipe Name: " + recipeName)
+console.log("Preparation Time: " + preparationTime)
+console.log("Recipe By: " + recipeBy)
+console.log("Is Vegetarian: " + isVegetarian)
+console.log("calories: " + calories)
